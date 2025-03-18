077211b024b203ac16b94c4fc3f23a1a1a9c076d
mwanavina <mmwanavina@gmail.com>
2025-03-18 10:59:27 +0200

    first commit

---
+++ src/question2/EvenSumCalculator.java
@@ -0,0 +1,13 @@
+package question2;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(3)); // "Fizz"
+        System.out.println(fizzBuzz(5)); // "Buzz"
+        System.out.println(fizzBuzz(15)); // "FizzBuzz"
+        System.out.println(fizzBuzz(4)); // "4"
+    }
+
+}
